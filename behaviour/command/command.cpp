31333b101ec47837a4461b05874d594b9133f99c
liu haixiang <haixiang1977@yahoo.com>
2016-11-21 22:50:09 +0800

    understanding command design pattern
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

---
+++ behaviour/command/command.cpp
@@ -0,0 +1,15 @@
+/*
+ * command.cpp
+ *
+ *  Created on: Nov 17, 2016
+ *      Author: liu
+ */
+/* http://www.codeproject.com/Articles/343676/Understanding-and-Implementing-the-Command-Pattern */
+/* In object-oriented programming, the command pattern is a behavioral design
+ * pattern in which an object is used to encapsulate all information needed to
+ * perform an action or trigger an event at a later time. This information
+ * includes the method name, the object that owns the method and values for the
+ * method parameters
+ */
+
+
