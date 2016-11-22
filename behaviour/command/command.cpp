f2b6c9f8f569aeafa19b01cc5f0d0b43363c018a
liu haixiang <haixiang1977@yahoo.com>
2016-11-22 22:35:51 +0800

    understanding the command design pattern
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

--- behaviour/command/command.cpp
+++ behaviour/command/command.cpp
@@ -12,4 +12,111 @@
  * method parameters
  */
 
+/* Command: This is an interface for executing an action. */
+/* ConcreteCommand: This object specifies the binding between a Receiver/action
+ * taker and an action invoker. */
+/* Client: Creates a ConcreteCommand object and sets its receiver. */
+/* Invoker: It will use the Command object to carry out the request. */
+/* Receiver: It contains the real operational logic that needs to be performed
+ * on the data. */
 
+#include <iostream>
+
+using namespace std;
+
+enum ACTION_LIST {
+	ADD,
+	SUBSTRACT,
+	MULTIPLY,
+	NONE
+};
+
+class IReceiver {
+public:
+	virtual ~IReceiver() {}
+	virtual void SetAction(ACTION_LIST action) = 0;
+	virtual int GetResult() = 0;
+};
+
+class ACommand {
+protected:
+	IReceiver *pReceiver_;
+
+public:
+	ACommand (IReceiver *receiver): pReceiver_(receiver) {}
+	virtual ~ACommand() {}
+	virtual int Execute() = 0;
+};
+
+class AddCommand : public ACommand {
+public:
+	AddCommand (IReceiver *receiver): ACommand (receiver) {}
+	int Execute() {
+		pReceiver_->SetAction(ADD);
+		return pReceiver_->GetResult();
+	}
+};
+
+class SubCommand : public ACommand {
+public:
+	SubCommand (IReceiver *receiver): ACommand (receiver) {}
+	int Execute() {
+		pReceiver_->SetAction(SUBSTRACT);
+		return pReceiver_->GetResult();
+	}
+};
+
+class MultiplyCommand : public ACommand {
+public:
+	MultiplyCommand (IReceiver *receiver): ACommand (receiver) {}
+	int Execute() {
+		pReceiver_->SetAction(MULTIPLY);
+		return pReceiver_->GetResult();
+	}
+};
+
+class Caculator : public IReceiver {
+private:
+	int x_;
+	int y_;
+	ACTION_LIST current_action;
+
+public:
+	Caculator (int x, int y) : x_(x), y_(y), current_action(NONE) {}
+	void SetAction (ACTION_LIST action) { current_action = action;}
+	int GetResult() {
+		int result;
+
+		switch (current_action) {
+		case (ADD):
+			result = x_ + y_;
+			break;
+		case (SUBSTRACT):
+			result = x_ - y_;
+			break;
+		case (MULTIPLY):
+			result = x_ * y_;
+			break;
+		default:
+			result = 0xFFFFFFFF;
+			break;
+		}
+
+		return result;
+	}
+};
+
+int main ()
+{
+	Caculator c = Caculator(30, 20);
+
+	AddCommand* addcmd = new AddCommand(&c);
+	SubCommand* subcmd = new SubCommand(&c);
+	MultiplyCommand* multicmd = new MultiplyCommand(&c);
+
+	cout << "add result " << addcmd->Execute() << endl;
+	cout << "sun result " << subcmd->Execute() << endl;
+	cout << "multi result " << multicmd->Execute() << endl;
+
+	return 0;
+}
